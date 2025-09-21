eb99feba1f5b5befcf97c76467fb79302d8a5629
gaddganepola <ganeypola@gmail.com>
2025-09-21 11:54:02 +0530

    first commit

---
+++ src/test/java/com/ddg/EC2DeployDemo/Ec2DeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddg.EC2DeployDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2DeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
